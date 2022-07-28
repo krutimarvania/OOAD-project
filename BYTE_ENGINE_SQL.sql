023f4183888a14c563e8ba17220e7eb783006265
krutimarvania <46553932+krutimarvania@users.noreply.github.com>
2022-07-28 14:45:38 -0500

    Update ByteEngine Sql

--- BYTE_ENGINE_SQL.sql
+++ BYTE_ENGINE_SQL.sql
@@ -1,3 +1,3 @@
-use cyberminer_db;
+use ByteEngine_db;
 create table data ( title varchar(200), url varchar(200), description varchar(500));
-SELECT * FROM cyberminer_db.data;
+SELECT * FROM ByteEngine.data;
